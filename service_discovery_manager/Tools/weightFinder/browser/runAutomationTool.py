537ad3bb2a057488c32e887752680ca38eb14add
Seikwon Kim <seikwon.kim@samsung.com>
2019-05-09 19:50:09 +0900

    The codes for modifying HW and extract webpage loading time 1. Browserside code With chrome-remote interface, URL addr is sent to the browser process. The browser process loads webpage and calculates webpage loading time. Then HW feature and webpage loading time is written as CSV format for future training set. After calculating the loading time, the info to change HW feature is sent to the renderer side python code.
    2. Rendererside code
    CPU frequency is controlled with DVFS, network latency and bandwidth is controlled with qc control.
    Once HW feature is received from browserside code, the HW is configured in the rendererside.

---
+++ service_discovery_manager/Tools/weightFinder/browser/runAutomationTool.py
@@ -0,0 +1,88 @@
+#
+# Tool to run Automation testing.
+# Save the result as csv
+
+import sys, csv, socket, json
+from subprocess import check_output
+
+global g_ip
+global g_port
+
+global g_socket
+
+g_loadingTimeExtractor = './loadingtime_extract.js'
+g_availableFreq = [3200, 2500, 1800, 1100, 800]
+#g_availableFreq = [3200, 3000, 2900, 2700, 2500, 2300, 2200, 2000, 1800, 1700, 1500, 1300, 1100, 1000,800]
+g_numCores = 4
+
+g_maxNetDelay = 80 # max 80 ms delay
+g_NetDelayStep = 20 # 20 ms per step
+
+g_maxPacketDropRate = 10 # maximum 10%
+g_packetDropRateStep = 3 # 3% per step
+
+g_netBandwidth = 304857600 # 300Mbps as default. Modification needed after testing
+
+g_benchSiteFile = 'top-100.csv'
+g_resultFile = 'result.csv'
+
+
+def configRemoteNode(latency, packetDropRate, bigcoreFreq, littlecoreFreq, numBigCores, numLittleCores, cpuUtil): #network bandwidth, network bandwidth, cpu frequency, free memory
+    data = json.dumps({"latency": latency, "dropRate": packetDropRate, "frequency_big": bigcoreFreq, "numBigCores": numBigCores, "frequency_little": littlecoreFreq, "numLittleCores": numLittleCores, "cpuUtil": cpuUtil})
+    g_socket.send(data.encode())
+    g_socket.recv(1024) # receive Complete msg
+
+def loadAlexaPage(filename):
+    webpages = []
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        line_count = 0
+        for row in csv_reader:
+            webpages.append(row[1]);
+        return webpages;
+
+def calculateBandwidth(packetDropRate): #calculates bandwidth from packetDropRate
+    return (g_netBandwidth * (1 - float(packetDropRate)/100))
+
+def writeLoadingTime(url, latency, packetDropRate, bigcoreFreq, littlecoreFreq, numBigCores, numLittleCores, cpuUtil, csv_writer):
+    loadingTime = check_output([r'node', g_loadingTimeExtractor, url])
+    bandwidth = calculateBandwidth(packetDropRate)
+    csv_writer.writerow([url, latency, bandwidth, bigcoreFreq, littlecoreFreq, numBigCores,numLittleCores, cpuUtil, str(loadingTime.split()[0])])
+
+def _main():
+
+    global g_socket
+
+    g_socket = socket.socket()
+    g_socket.connect((g_ip, int(g_port)))
+
+    webpageList = loadAlexaPage(g_benchSiteFile)
+    csv_writer = csv.writer(open(g_resultFile, 'w'))
+    csv_writer.writerow(["URL", "latency(ms)", "bandwidth(bps)", "bigcoreFreq", "littlecoreFreq", "numBigCores","numLittleCores", "cpu Utilization", "loadingTime(ms)"])
+    prefix = "http://www."
+    
+
+    for bfreq in g_availableFreq[0:]: #big core frequency
+        for lfreq in g_availableFreq[1:]: #little core frequency
+            if bfreq > lfreq: # big freq should be larger than little freq
+               for numLittleCore in range(0, g_numCores, 1): #numCores range between 0 ~ numCore-1
+                   for cpuUtil in range(0, 80, 20): # for CPU usage 0% to 90%
+                       for netDelay in range(0, g_maxNetDelay, g_NetDelayStep): # for network delay 0 to 5sec
+                           for netPacketDropRate in range(0, g_maxPacketDropRate, g_packetDropRateStep): # 
+                               for wp in webpageList: # for each webpage
+ #                                  print prefix+wp +  " " + str(bfreq) +" "+ str(lfreq)+ " " + str(g_numCores-numLittleCore) +" "  + str(numLittleCore) + " " +str(cpuUtil)+ " " + str(netDelay) + " " + str(calculateBandwidth(netPacketDropRate))
+                                   configRemoteNode(netDelay, netPacketDropRate, bfreq, lfreq, g_numCores - numLittleCore, numLittleCore, cpuUtil)                                  
+                                   writeLoadingTime(prefix+wp, netDelay, netPacketDropRate, bfreq, lfreq, g_numCores - numLittleCore, numLittleCore, cpuUtil, csv_writer)
+
+    g_socket.close()
+
+if __name__ == "__main__":
+    if (len(sys.argv) < 3):
+        print("Usage: python " + __file__ + " [server_ip] [server_port]")
+    else:
+        global g_ip
+        global g_port
+        g_ip = sys.argv[1]
+        g_port = sys.argv[2]
+
+        _main()
---
+++ service_discovery_manager/Tools/weightFinder/renderer/server.py
@@ -0,0 +1,214 @@
+#
+# Sets renderer configs on Ubuntu 16.04 
+# cpufreq-info, cpulimit command must be avaliable to run the Automation code
+# edit GRUB_CMDLINE_LINUX_DEFAULT in /etc/default/grub and add 'intel_pstate=disable'
+# 'sudo update-grub' must be performed after editing GRUB_CMDLINE_LINUX_DEFAULT
+# and must reboot after setting new grub
+# Prior to run the code, chrome renderer should be running
+#
+# This auto-test code is only tested on Ubuntu 16.04
+# Use it at your own risk and feel free to modify the code
+#
+
+import socket, json, atexit, os, sys
+from subprocess import Popen, PIPE
+from subprocess import check_output
+
+global g_port
+
+g_availableFreq = [3200, 3000, 2900, 2700, 2500, 2300, 2200, 2000, 1800, 1700, 1500, 1300, 1100, 1000, 800] # available MHz by cpufrq-info cmd
+g_procName = "chrome"
+g_cgroupDir = "/dev/cgroups"
+g_groupMemLimitDir = g_cgroupDir+"/limitGroup"
+
+g_numCores = 1 # modifies at init
+g_chromePIDs = []
+
+def exit_handler():
+    cmd = 'tc qdisc del dev enp4s0 root'
+    os.system(cmd)
+    cmd = 'killall cpulimit'
+    os.system(cmd)
+    return
+
+def get_pid(name):
+    g_chromePIDs =  map(int,check_output(["pidof",name]).split())
+    return map(int,check_output(["pidof",name]).split())
+    #print g_chromePIDs;
+
+def setNetworkConfig(latency, packetDropRate):
+    cmd = 'tc qdisc replace dev enp4s0 root handle 1:0 netem loss '+str(packetDropRate)+'% delay ' + str(latency) +'ms'
+    os.system(cmd)
+    return
+
+'''
+def setNetworkLatency(latency):
+    print ("Set Latnecy: "+str(latency))
+    cmd = 'tc qdisc replace dev enp4s0 root handle 1:0 netem delay ' + str(latency)+'ms'
+    print cmd
+    os.system(cmd)
+    return
+
+def setNetworkBandwidth(packetDropRate): #since only egress can be controlled and ingress cannot be controled, we control the bandwidth with packet drop rate.
+    print "Set packetDropRate: " + str(packetDropRate)
+    cmd = 'tc qdisc replace dev enp4s0 parent 1:0 handle 10: netem loss ' + str(packetDropRate)+'%'
+    print cmd
+    os.system(cmd)
+    return
+'''
+
+def setCPUUtilization(utilization):
+    utilization = (100 - utilization)*g_numCores # limit cpu usage (Current CPU utilization 30% = my proc can use maximum 70% of the CPU)
+    cmd = 'killall cpulimit'
+    os.system(cmd)
+    
+    for i in g_chromePIDs:
+        cmd = 'cpulimit -b -l ' + str(utilization) + ' -p ' + str(i)
+        #print cmd
+        os.system(cmd)
+    return
+
+def setCPUFrequency(numBigCore, bigCoreFreq, numLittleCore, littleCoreFreq):
+    numCores = numBigCore + numLittleCore
+    for i in range(1,g_numCores): # turn on Cores
+        cmd = 'echo 1 | tee /sys/devices/system/cpu/cpu' + str(i) +'/online'
+        os.system(cmd)
+    for i in range(numCores, g_numCores): # turn off Rest of the cores
+        cmd = 'echo 0 | tee /sys/devices/system/cpu/cpu' + str(i) +'/online'
+        os.system(cmd)
+
+    for i in range(0, numBigCore):
+        if (bigCoreFreq in g_availableFreq): #frequency must exist in the available list
+            cmd = 'cpufreq-set -c ' +str(i)+ ' -u ' + str(bigCoreFreq)+ 'MHz'
+            os.system(cmd)
+    for i in range(numBigCore, numBigCore + numLittleCore):
+        if (littleCoreFreq in g_availableFreq):
+            cmd = 'cpufreq-set -c ' +str(i)+ ' -u ' + str(littleCoreFreq)+ 'MHz'
+            os.system(cmd)
+    return
+
+def setNumCores(numCores): # turn off some cores
+    if (g_numCores <= numCores):
+        return
+    else:
+        for i in range(1,g_numCores):
+            cmd = 'echo 1 | tee /sys/devices/system/cpu/cpu' + str(i) +'/online'
+            os.system(cmd)
+        for i in range(numCores, g_numCores): # cores to turn off            
+            cmd = 'echo 0 | tee /sys/devices/system/cpu/cpu' + str(i) +'/online'
+            os.system(cmd)
+    return
+
+'''
+# Memory setting removed since Android supports OOMK. OOMK kills process if memory is not enough
+def setMemory(memSizeMB):
+    memSizeByte = memSizeMB * 1024 * 1024
+    cmd = "echo "+ str(memSizeByte) + " | tee " + g_groupMemLimitDir + "/memory.limit_in_bytes"
+    print cmd
+    os.system(cmd);
+    cmd = "echo "+ str(memSizeByte) + " | tee " + g_groupMemLimitDir + "/memory.memsw.limit_in_bytes"
+    print cmd
+    os.system(cmd);
+    return
+
+def addChromeToCgroupLimit():
+    for i in g_chromePIDs:
+        # echo <PID> > /dev/cgroups/test/tasks
+        os.system("echo "+ str(i) + " | tee "+ g_groupMemLimitDir + "/tasks")
+    return
+'''
+
+def initAutoConfig():
+    global g_numCores
+    global g_chromePIDs
+    '''
+    os.system("mkdir " + g_cgroupDir)
+    os.system("mount -t cgroup -omemory memory "+g_cgroupDir)
+    os.system("mkdir " + g_groupMemLimitDir)
+    '''
+
+    process = Popen([r'nproc','--all'], stdin=PIPE, stdout=PIPE)
+    g_numCores = int(process.stdout.readline().rstrip())
+    g_chromePIDs = get_pid(g_procName);
+
+    return
+
+def _main():
+
+    permission = os.getuid()
+    if permission != 0:  #check if the testCode is executed with root permission
+        print "Please execute with root permission"
+        return
+
+    initAutoConfig()    
+    #setCPUUtilization(60)
+    atexit.register(exit_handler)
+
+
+    s = socket.socket()         # Create a socket object
+    port = g_port                # Reserve a port for your service.
+    s.bind(('0.0.0.0', int(port)))        # Bind to the port
+    s.listen(5)                 # Now wait for client connection.
+    prev_nwDropRate = -1
+    prev_nwLatency = -1
+    prev_cpuFrequency_big = -1
+    prev_cpuFrequency_little = -1
+    prev_numBigCores = -1 
+    prev_numLittleCores = -1 
+    prev_freeMemMB = -1
+    prev_cpuUtilization = -1
+    
+    c, addr = s.accept()     # Establish connection with client.
+
+    while True:
+        print 'Got connection from', addr
+        data = c.recv(1024)
+        data = json.loads(data.decode())
+
+        #Network Parameters
+        latency = data.get("latency") # add latency to send packet
+        dropRate = data.get("dropRate") #control packet drop rate to control bandwidth
+
+        #CPU parameters
+        cpuFrequency_big = data.get("frequency_big") # Big core frequency
+        cpuFrequency_little = data.get("frequency_little") # Little core frequency
+        numBigCores = data.get("numBigCores")
+        numLittleCores = data.get("numLittleCores")
+        cpuUtilization = data.get("cpuUtil")
+
+       
+        print str(latency) + " " + str(dropRate)  + " " + str(cpuFrequency_big) + " " + str(cpuFrequency_little) + " " + str(numBigCores) + " " + str(numLittleCores) + " " + str(cpuUtilization) 
+        
+        if ((latency != prev_nwLatency) or (dropRate != prev_nwDropRate)): 
+            setNetworkConfig(latency, dropRate)
+        if ((numBigCores != prev_numBigCores) or (numLittleCores != prev_numLittleCores) or (prev_cpuFrequency_big != prev_cpuFrequency_big) or (prev_cpuFrequency_little != cpuFrequency_little)):
+            setCPUFrequency(numBigCores, cpuFrequency_big, numLittleCores, cpuFrequency_little)
+        if(cpuUtilization != prev_cpuUtilization):
+            setCPUUtilization(cpuUtilization)
+        
+        #if freeMemMB != prev_freeMemMB:
+        #    setMemory(freeMemMB)
+        c.send("Complete")
+        
+        prev_nwDropRate = dropRate
+        prev_nwLatency = latency
+
+        prev_cpuFrequency_big = cpuFrequency_big
+        prev_cpuFrequency_little = cpuFrequency_little
+        prev_numBigCores = numBigCores
+        prev_numLittleCores = numLittleCores
+        prev_cpuUtilization = cpuUtilization
+
+        #prev_freeMemMB = freeMemMB
+    c.close()
+
+
+if __name__ == "__main__":
+    if (len(sys.argv) < 2):
+        print("Usage: python " + __file__ + " [port]")
+    else:
+        global g_port
+        g_port = sys.argv[1]
+
+        _main()
+
