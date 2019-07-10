502a614d1ce29dbc6fb11f09a8f7e80cfdf84977
Seikwon Kim <seikwon.kim@samsung.com>
2019-07-10 15:35:45 +0900

    error modificatio

--- content/browser/service_manager/service_manager_context.cc
+++ content/browser/service_manager/service_manager_context.cc
@@ -122,8 +122,6 @@ void StartServiceInUtilityProcess(
     service_manager::mojom::ConnectResult query_result,
     const std::string& sandbox_string) {
 	
-	travisBuildFailCheckOnPurpose = abc;
-
   service_manager::SandboxType sandbox_type =
       service_manager::UtilitySandboxTypeFromString(sandbox_string);
 
