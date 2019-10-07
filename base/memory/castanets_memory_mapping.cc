96d7065096698df6420e1c4381c7878ead456099
Hyowon Kim <hw1008.kim@samsung.com>
2019-10-07 15:06:28 +0900

    Fix a warning about passing the value after munmap.
    This is a warning from Svace. Value 'memory' passed to procedure
    at castanets_memory_mapping.cc:56 after release at
    castanets_memory_mapping.cc:55 by calling function 'munmap'.

--- base/memory/castanets_memory_mapping.cc
+++ base/memory/castanets_memory_mapping.cc
@@ -52,8 +52,8 @@ void* CastanetsMemoryMapping::MapForSync(int fd) {
 
 void CastanetsMemoryMapping::UnmapForSync(void* memory) {
   CHECK(memory);
-  munmap(memory, mapped_size_);
   RemoveMapping(memory);
+  munmap(memory, mapped_size_);
 }
 
 }  // namespace base
