582ab46968c99dd7e7467d5e44e48b660a913786
is46-kim <is46.kim@samsung.com>
2020-12-14 12:14:28 +0900

    Add Castanets Internal Repo for Settings UI (#383)
    Signed-off-by: Insoon Kim <is46.kim@samsung.com>

--- build/update_internal_repo.sh
+++ build/update_internal_repo.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+[ -d third_party/castanets-internal ] || git clone git@github.sec.samsung.net:HighPerformanceWeb/castanets-internal.git third_party/castanets-internal
+git -C third_party/castanets-internal pull
+
 [ -d third_party/offload-js ] || git clone git@github.sec.samsung.net:HighPerformanceWeb/offload.js.git third_party/offload-js
 git -C third_party/offload-js pull
