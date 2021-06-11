3901f3348358417f2a91dc333a17a50dc3e63e2e
v-saha <v.saha@samsung.com>
2021-06-11 15:18:34 +0530

    Enable to play video with MediaElement
    Synchronize the gpu memory buffer in the video frame resource.
    In case of a SW compositor, synchronize the SW resource.
    Set the frame time to the system time of renderer.
    
    Signed-off-by: v-saha <v.saha@samsung.com>

--- media/video/gpu_memory_buffer_video_frame_pool.cc
+++ media/video/gpu_memory_buffer_video_frame_pool.cc
@@ -39,6 +39,10 @@
 #include "ui/gfx/color_space.h"
 #include "ui/gl/trace_util.h"
 
+#if defined(CASTANETS)
+#include "mojo/public/cpp/system/platform_handle.h"
+#endif
+
 #if defined(OS_MACOSX)
 #include "base/mac/mac_util.h"
 #include "ui/gfx/mac/io_surface.h"
@@ -690,6 +694,11 @@ void GpuMemoryBufferVideoFramePool::PoolImpl::OnCopiesDone(
     FrameResources* frame_resources) {
   for (const auto& plane_resource : frame_resources->plane_resources) {
     if (plane_resource.gpu_memory_buffer) {
+#if defined(CASTANETS)
+      gfx::GpuMemoryBuffer *buffer = plane_resource.gpu_memory_buffer.get();
+      mojo::SyncSharedMemoryHandle(buffer->CloneHandle().region.GetGUID(), 0,
+                                   buffer->CloneHandle().region.GetSize());
+#endif
       plane_resource.gpu_memory_buffer->Unmap();
       plane_resource.gpu_memory_buffer->SetColorSpace(
           video_frame->ColorSpace());
